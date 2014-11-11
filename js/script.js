a23aca5be83d1e836b80808061fb24d987091bff
Anna <anna@safronovs.com>
2014-11-11 16:24:55 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,18 +1,73 @@
+ var submitBtn = document.getElementById('submitBtn');
+var output = document.getElementById('output');
 
-var submit = document.getElementById('comb').addEventListener('click', function(e) {
-  
-  
-  // Input:
-  var data = 'input=' + document.getElementById('input').value;
-  
-  // Request:
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function(){
-    if (req.readyState === 4) {
-        document.getElementById('output').value = req.responseText;
+
+//keep shorthands in array
+ var shorthands = [
+        'animation',
+        'transition',
+        'background',
+        'border',
+        'border-color',
+        'border-style',
+        'border-width',
+        'border-top',
+        'border-right',
+        'border-bottom',
+        'border-left',
+        'border-image',
+        'border-radius',
+        'columns',
+        'column-rule',
+        'flex',
+        'font',
+        'font-variant',
+        'list-style',
+        'margin',
+        'padding',
+        'outline',
+        'text-decoration'
+    ];
+
+
+function removeSpaces(){
+
+  var userCSS = document.getElementById('input').value;
+  console.log(userCSS);
+
+  for(i=0; i>shorthands.length; i++){
+    if(userCSS !== shorthands[i]){
+
+      userCSS = userCSS.split(' ').join('');
+      userCSS = userCSS.replace(/\s/g, '');
+      console.log(userCSS);
+
     }
-  };
-  req.open('POST', '/online', true);
-  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  req.send(data);
-});
+
+  }
+
+  
+
+  // onblur="this.value=removeSpaces(this.value);"
+
+  //console.log(output);
+  //output.value = userCSS;
+
+
+
+}
+
+ // submitBtn.addEventListener("click", removeSpaces);
+
+
+//write checking function based on punctuation
+// store 'px', 'em', 'rem', 'vh', 'vm' as one unit each
+// remove all spaces except in shorthands
+// check shorthand for double space and remove it
+//
+
+
+
+
+
+
