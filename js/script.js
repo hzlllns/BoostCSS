38add21a91027b2bd9882c9d91ddc0a452ff9ad8
Anna <anna@safronovs.com>
2014-11-14 22:15:21 -0500

    output + comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+var inputText = document.getElementById('input');
+var outputText = document.getElementById('output');
+
+inputText.setAttribute('spellcheck', 'false');
+
+
 //clean array elements by value
 Array.prototype.clean = function(deleteValue) {
   for (var i = 0; i < this.length; i++) {
@@ -9,60 +15,74 @@ Array.prototype.clean = function(deleteValue) {
   return this;
 };
 
-
 var cssTool = function(){
 
   var self = this;
-  //create array for css(?)
+
+  //cssRule array
   self.list = [];
 
-  //turn css into string(?)
+  //original css
   self.originalCss = "";
 
-  //reset default button's setting 
-  //use do function on input css  
+  //submit button callback
   self.cssSubmit = function(e) {
     e.preventDefault();
-    self.do(document.getElementById('input').value);
+    self.do(inputText.value);
   } 
 
-  //implement all functions
+  //main function
   self.do = function(originalCss) {
 
-    //create empty array to push elements later 
+    //reset cssRule array 
     self.list = [];
 
     //store original css
     self.originalCss = originalCss;
 
-    //store css with removed line breaks
+    //strip css
     var stripedCss = self.stripCss(originalCss);
 
-    //store css chunks 
+    //split css by "}" 
     var dirtyElements = self.breakCssToElements(stripedCss);
 
-    //push chunks in list[]
+    //import CSS elements
     self.importElements(dirtyElements);
 
-    //start sort css
+    //sort css
     self.sortRules();
 
-    //the list at this point is sorted
-    //TODO: output the list
-    console.log(self.list);
-  }
+    //output formated css
+    self.outputCss();
 
-  //remove line breaks
+  }
+    //format and output CSS to textarea
+    self.outputCss = function() {
+
+        var output = "";
+
+        for (var i = 0; i < self.list.length; i++) {
+            output = output + self.list[i].formatCss();
+
+            if(i+1 != self.list.length) {
+                output = output + "\n\n";
+            }
+        };
+
+        outputText.value = output;
+    }
+
+  //trim trailing space
   self.stripCss = function(originalCss){
-    return originalCss.replace(/\n/g, '');
+    return originalCss.trim();
   }
 
-  //divide css into parts -> one selector = one part
+  //split css by "}"
   self.breakCssToElements = function(stripCSS) {
       return stripCSS.split('}').clean("");
   }
 
-  //function to store css in array
+  //import css elements
   self.importElements = function(dirtyElements) {
 
     for (var i =  0; i < dirtyElements.length; i++) {
@@ -70,51 +90,76 @@ var cssTool = function(){
     };
   }
 
-  //change css order
+  //sort css properties
   self.sortRules = function(){
     for (var i = 0; i < self.list.length; i++) {
       self.list[i].sortRules();
     };
   }
 
-  //start cssSubmit function after the button is clicked
   document.getElementById('submitBtn').addEventListener('click', self.cssSubmit); 
 
 }
 
-//create rules for sorting css
+
+
+
+//css object that contains selector(s) and properties (key: value) + comment
 var cssRule = function(dirtyElement){
+
   var self = this;
+
+  self.comments = [];
 
   //create array for selectors
   self.selectors = [];
+
   //create array for properties
   self.properties = [];
 
-  //implement all functions
+  //main function
   self.cleanDirtyElement = function(dirtyElement){
 
     var tmp = self.breakElement(dirtyElement);
 
-    self.parseSelectors(tmp[0]);
+    self.parseComment(tmp[0]);
 
-    self.parseProperties(tmp[1]);
+    self.parseSelectors(tmp[1]);
+
+    self.parseProperties(tmp[2]);
   }
 
-  //divide strings to selectors and properties
+  //split element's string to comments, selectors and properties
   self.breakElement = function(dirtyElement){
 
     //remove extra spaces
     dirtyElement = dirtyElement.trim();
 
-    //split css by '{' and remove empty elements
-    return dirtyElement.split('{').clean("");
+    //split css by '{' and remove empty array elements
+    var tmp = dirtyElement.split('{').clean("");
+    var tmp2 = tmp[0].split("*/").clean("");
+
+    var properties = tmp[1];
+
+    if(tmp2[1] !== undefined){
+        var comment = tmp2[0];
+        var selectors = tmp2[1];
+    } else {
+        var comment = null;
+        var selectors = tmp2[0];
+    }
+
+    return [comment, selectors, properties];
+
+
   }
 
-  //divide multiple selectors
+  //break selector's string to array
   self.parseSelectors = function(dirtySelectors){
 
-    //remove spaces in selectors
+    dirtySelectors = dirtySelectors.replace(/\n/g, '');
+
+    //remove extra spaces
     dirtySelectors = dirtySelectors.trim();
 
     //split selectors by ',' to array and clean empty elements
@@ -122,22 +167,22 @@ var cssRule = function(dirtyElement){
 
   }
 
-  //divide properties to one line "key: value"
+  //break properties to array of objects with key and value
   self.parseProperties = function(dirtyProperties){
+
+    dirtyProperties = dirtyProperties.replace(/\n/g, '');
 
     //remove extra space
     dirtyProperties = dirtyProperties.trim();
 
-    //divide properties to single lines
     var tmpProperties = dirtyProperties.split(';').clean("");
 
-    //call function to split one line
     for (var i = 0; i < tmpProperties.length; i++) {
         self.parseProperty(tmpProperties[i]);
     };
   }
 
-  //divide property to keys and values
+  //break property to key and value
   self.parseProperty = function(dirtyProperty){
 
     //remove extra spaces
@@ -146,17 +191,40 @@ var cssRule = function(dirtyElement){
     //divide string to keys and values
     var tmp = dirtyProperty.split(':').clean("");
 
-    //push clean elements to array object
-    self.rules.push({ key: tmp[0].trim(), value: tmp[1].trim() });
+    //push clean elements to array
+    self.properties.push({ key: tmp[0].trim(), value: tmp[1].trim() });
 
   }
 
-  //sort compared keys
-  self.sortProperties = function() {
+  //break comments string to array
+  self.parseComment = function(dirtyComment){
+
+    if(dirtyComment !== null){
+        dirtyComment = dirtyComment.replace('/*', '');
+        dirtyComment = dirtyComment.replace(/\*/g, '');
+        var comments = dirtyComment.split("\n").clean("");
+
+
+        for(i = 0; i < comments.length; i++){
+            comments[i] = comments[i].trim();
+        }
+
+        self.comments = comments;
+
+    } else {
+        self.comments = null;
+    }
+
+    
+
+  }
+
+  //sort properties
+  self.sortRules = function() {
     self.properties.sort(self.compare);
   }
 
-  //compare keys with json 
+  //sort callback, compare indexOf properties' keys in css order array
   self.compare = function(a, b){
     if(cssOrder.indexOf(a.key) < cssOrder.indexOf(b.key)){
       return -1;
@@ -166,9 +234,73 @@ var cssRule = function(dirtyElement){
     return 0;
   }
 
+  //output method
+    self.formatCss = function(){
+
+        var temp = self.formatComment();
+        temp = temp + self.formatSelectors();
+        temp = temp + self.formatProperties();
+        
+        return temp;
+    }
+
+    //format selectors: add line breaks, commas and etc
+    self.formatSelectors = function(){
+        var temp = "";
+
+        for(i=0; i < self.selectors.length; i++) {
+            temp = temp + self.selectors[i];
+             if( i+1 == self.selectors.length){
+                temp = temp + " {\n";
+             }else {
+                temp = temp + ",\n";
+             }
+        }
+
+        return temp;
+
+    }
+
+    //format properties: add line breaks, colun and etc
+    self.formatProperties = function() {
+        var temp = "";
+
+        for(i=0; i < self.properties.length; i++) {
+            temp = temp + "\t" + self.properties[i].key + ": " + self.properties[i].value + ";\n";
+            if(i+1 == self.properties.length) {
+                temp = temp + "}";
+            }
+        }
+
+        return temp;
+
+    }
+
+    //format comments: add line breaks, * and etc
+    self.formatComment = function(){
+        if(self.comments !== null){
+
+            var temp = "/*\n";
+
+            for(i=0; i < self.comments.length; i++) {
+                temp = temp + " * " + self.comments[i] + "\n";
+                
+                if(i+1 == self.comments.length) {
+                    temp = temp + " */\n";
+                }
+            }
+
+
+            return temp;
+        }
+
+        return "";
+    }
+
   self.cleanDirtyElement(dirtyElement);
 
 }
+
 
 //create an instance 
 var tool = new cssTool();
